4543b019a9c7c686bc72ecc3333e770847fac423
NiceAndFlowy <NiceAndFlowy@user.noreply.github.com>
2017-05-27 21:10:41 -0400

    Dynamic Rendering & Query Params

--- app/components/App.js
+++ app/components/App.js
@@ -6,7 +6,8 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Nav = require('./Nav');
 var Home = require('./Home');
-var Battle = require('./Battle')
+var Battle = require('./Battle');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -17,6 +18,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found</p>
